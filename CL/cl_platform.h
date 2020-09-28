134b005b9b0cf850e85e5364e3209c624f093bc9
Nagy-Egri Máté Ferenc <nagy-egri.mate@wigner.mta.hu>
2020-09-28 18:07:01 +0200

    Add popping warning for /Za builds

--- CL/cl_platform.h
+++ CL/cl_platform.h
@@ -1377,7 +1377,7 @@ typedef union
 
 #undef __CL_HAS_ANON_STRUCT__
 #undef __CL_ANON_STRUCT__
-#if defined( _WIN32) && defined(_MSC_VER)
+#if defined( _WIN32) && defined(_MSC_VER) && ! defined(__STDC__)
     #if _MSC_VER >=1500
     #pragma warning( pop )
     #endif
