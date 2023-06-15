f5893a8be50b84709eacc6282c802420ce10a372
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2023-06-15 14:41:17 +0000

    Fixes from review: now packaging the install tree; Debian package name and installation prefix fixed

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -1,6 +1,6 @@
 set(CPACK_PACKAGE_VENDOR "khronos")
 
-set(CPACK_PACKAGE_DESCRIPTION "OpenCL API C headers")
+set(CPACK_PACKAGE_DESCRIPTION "OpenCL (Open Computing Language) C header files")
 
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 
@@ -10,34 +10,31 @@ if(NOT CPACK_PACKAGING_INSTALL_PREFIX)
   set(CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 endif()
 
-if(NOT CPACK_SOURCE_IGNORE_FILES)
-  set(CPACK_SOURCE_IGNORE_FILES="/.github/;/build/")
-endif()
-
 # DEB packaging configuration
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER ${CPACK_PACKAGE_VENDOR})
 
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE
     "https://github.com/KhronosGroup/OpenCL-Headers")
 
-# Get Debian package name from project name
-string(TOLOWER ${PROJECT_NAME} DEBIAN_PACKAGE_NAME)
-string(REPLACE "opencl" "opencl-" DEBIAN_PACKAGE_NAME ${DEBIAN_PACKAGE_NAME})
-
 # Version number [epoch:]upstream_version[-debian_revision]
-set(CPACK_DEBIAN_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION}) # upstream_version
-set(CPACK_DEBIAN_PACKAGE_RELEASE "1") # devian_revision (because this is a
+set(LATEST_RELEASE_VERSION "2023.04.17")
+set(CPACK_DEBIAN_PACKAGE_VERSION "${PROJECT_VERSION}~${LATEST_RELEASE_VERSION}")  # upstream_version
+set(CPACK_DEBIAN_PACKAGE_RELEASE "1") # debian_revision (because this is a
                                       # non-native pkg)
+set(PACKAGE_VERSION_REVISION "${CPACK_DEBIAN_PACKAGE_VERSION}-${CPACK_DEBIAN_PACKAGE_RELEASE}")
 
+set(DEBIAN_PACKAGE_NAME "opencl-c-headers")
 set(CPACK_DEBIAN_PACKAGE_NAME
-    "${CPACK_PACKAGE_VENDOR}-${DEBIAN_PACKAGE_NAME}"
+    "${DEBIAN_PACKAGE_NAME}"
     CACHE STRING "Package name" FORCE)
 
-set(CPACK_DEBIAN_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}")
+# Get architecture
+execute_process(COMMAND dpkg "--print-architecture" OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
+string(STRIP "${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}" CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
 
 # Package file name in deb format:
 # <PackageName>_<VersionNumber>-<DebianRevisionNumber>_<DebianArchitecture>.deb
-set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_FILE_NAME "${CPACK_PACKAGE_VENDOR}-${DEBIAN_PACKAGE_NAME}_${PACKAGE_VERSION_REVISION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}.deb")
 
 set(CPACK_DEBIAN_PACKAGE_DEBUG ON)
 
