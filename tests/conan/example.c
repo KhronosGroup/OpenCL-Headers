2c7964946818788620ccf57cd2a797de0f0ff5ed
yuri-schmaltz <yuri.schmaltz@gmail.com>
2025-12-16 01:34:52 -0300

    edit

--- tests/conan/example.c
+++
@@ -1,9 +0,0 @@
-#define CL_TARGET_OPENCL_VERSION 220
-#include <CL/cl.h>
-
-int main(void) {
-	cl_platform_id id;
-	cl_int result;
-	result = 0;
-	return result;
-}
--- tests/pkgconfig/pkgconfig.c
+++
@@ -1,3 +0,0 @@
-#include <CL/cl.h>
-
-int main(void) { return sizeof(cl_platform_id) - sizeof(cl_context); }
